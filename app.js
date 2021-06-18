22cf1f7347b8c06d0517ccbd6a84019dae7248fd
Mohamed G. Abdulkarim <mohammed.g.abdulkarim@outlook.com>
2021-06-18 02:05:23 +0200

    Study Session 18/06/2021

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const express = require('express');
 // Import the fs module
 const fs = require('fs');
+// Import Morgan module, it's a good module for logging information about a certain request.
+const morgan = require('morgan');
 
 // Assign the express functions to app variable in order to use all express functions.
 const app = express();
@@ -12,12 +14,32 @@ const app = express();
 const TOURS_SIMPLE_FILE = `${__dirname}/dev-data/data/tours-simple.json`;
 
 /**
- * Adding Middleware to the app.
+ * Adding @Middleware to the app.
  * Middleware is a function that can modify the incoming request data.
  * Stands between requests and responses
  * It's a step that the request goes through while it's being processed, and that would be adding the data to the request from the body.
+ * Every request in the cycle goes through all middleware in the order there're defined.
  */
 app.use(express.json());
+
+// Example log : GET /api/v1/tours 200 3.001 ms - 8640
+app.use(morgan('dev'));
+
+/**
+ * We can create our own middleware by using the app.use method.
+ * The 3rd @param is alway @next
+ */
+app.use((req, res, next) => {
+  console.log('Hello from the middleware ❤');
+  // we have to always call the next() function.
+  next();
+});
+
+app.use((req, res, next) => {
+  //Gets time of request.
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 /**
  * We start defining @routes , which is how an application responds to client requests, so to a certain @url as well as the @HTTP @method
@@ -42,9 +64,13 @@ app.use(express.json());
 const tours = JSON.parse(fs.readFileSync(TOURS_SIMPLE_FILE));
 
 /**
- * @Get request for tours
+ * Define all @routeHandlers all together.
  */
-app.get('/api/v1/tours', (req, res) => {
+
+// Route handlers for tours
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -52,14 +78,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-/**
- * @Get request for a tour with ID
- * the /:x is a variable we can use.
- * we can do "/x/y/z?", the ? marks a parameter as optional
- */
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTourById = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
@@ -77,12 +98,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-/**
- * @POST request for a tour
- * */
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   //req.body is what we have access to because we added middleware.
   //   console.log(req.body);
 
@@ -99,14 +117,9 @@ app.post('/api/v1/tours', (req, res) => {
       },
     });
   });
-});
+};
 
-/**
- * @PATCH request for a tour
- * Patch request expects only the properies that should be updated in the object.
- * @PUT requests expect the entire object to be updated.
- */
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   // This is just more of the same code, we take the id, find the tour, and make the changes, then save the file again.
 
   if (req.params.id * 1 > tours.length) {
@@ -122,12 +135,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here...',
     },
   });
-});
+};
 
-/**
- * @DELETE request for a tour
- */
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   // This is just more of the same code, we take the id, find the tour, and make the changes, then save the file again.
 
   if (req.params.id * 1 > tours.length) {
@@ -142,7 +152,96 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+// Route handlers for users
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getUserById = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+/**
+ * @Get request for tours
+ */
+// app.get('/api/v1/tours', getAllTours);
+
+/**
+ * @Get request for a tour with ID
+ * the /:x is a variable we can use.
+ * we can do "/x/y/z?", the ? marks a parameter as optional
+ */
+// app.get('/api/v1/tours/:id', getTourById);
+
+/**
+ * @POST request for a tour
+ * */
+// app.post('/api/v1/tours', createTour);
+
+/**
+ * @PATCH request for a tour
+ * Patch request expects only the properies that should be updated in the object.
+ * @PUT requests expect the entire object to be updated.
+ */
+// app.patch('/api/v1/tours/:id', updateTour);
+
+/**
+ * @DELETE request for a tour
+ */
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+/**
+ * Another way to gather all actions and routes and make the code easier to maintain is to use the @route function of @app.
+ */
+
+// Routes for tours
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTourById)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// Routs for users.
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUserById)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // -------------------------------------------------------
 
